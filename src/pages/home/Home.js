17b0649e765420d8f0adf94028324b38931bd263
lxxulli <lyr98803@naver.com>
2023-11-15 16:08:59 +0900

    home 배너이미지, 반응형

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,61 +2,7 @@ import styled from "styled-components";
 import { nowPlaying } from "../../api";
 import { useEffect, useState } from "react";
 import { IMG_URL } from "../../constants";
-
-const MainBanner = styled.section`
-  height: 80vh;
-  background-color: lightgray;
-  position: relative;
-  padding: 400px 5%;
-  background: url(${IMG_URL}/original/${(props) => props.$bgUrl}) no-repeat
-    center / cover;
-
-  h3,
-  p {
-    position: relative;
-  }
-  h3 {
-    max-width: 650px;
-    width: 100%;
-    font-size: 80px;
-    font-weight: 700;
-    margin-bottom: 30px;
-    letter-spacing: -3px;
-    line-height: 100px;
-  }
-  p {
-    max-width: 650px;
-    width: 100%;
-    font-size: 18px;
-    font-weight: 400;
-    line-height: 26px;
-    opacity: 0.8;
-  }
-
-  @media screen and (max-width: 450px) {
-    h3 {
-      font-size: 50px;
-      line-height: 60px;
-    }
-    p {
-      font-size: 16px;
-    }
-  }
-`;
-
-const BlackBg = styled.div`
-  width: 100%;
-  height: 100%;
-  background: linear-gradient(
-    0deg,
-    rgba(0, 0, 0, 1) 0%,
-    rgba(94, 94, 94, 0.765625) 45%,
-    rgba(255, 255, 255, 0.7854166) 100%
-  );
-  position: absolute;
-  top: 0;
-  left: 0;
-`;
+import { Banner } from "./Banner";
 
 export const Home = () => {
   // nowPlaying(); //비동기 통신할 때 그냥 함수를 불러올 수가 없음 (이렇게 하면 안됨)
@@ -91,15 +37,7 @@ export const Home = () => {
         "loading..."
       ) : (
         // 그게 아니라면
-        <div>
-          {nowPlayingData && ( // 안전장치 1개 더
-            <MainBanner $bgUrl={nowPlayingData[0].backdrop_path}>
-              <BlackBg />
-              <h3>{nowPlayingData[0].title}</h3>
-              <p>{nowPlayingData[0].overview.slice(0, 100) + "..."}</p>
-            </MainBanner>
-          )}
-        </div>
+        <div>{nowPlayingData && <Banner data={nowPlayingData} />}</div>
       )}
     </>
   );
