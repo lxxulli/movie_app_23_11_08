ec643464363bb3c8c1155271a7cbd6f5019f04ff
lxxulli <lyr98803@naver.com>
2023-11-17 18:02:49 +0900

    detail 세부내용

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -1,9 +1,13 @@
-import { Helmet } from "react-helmet";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 export const PageTitle = ({ titleName }) => {
   return (
-    <Helmet>
-      <title> YUL | {titleName}</title>
-    </Helmet>
+    <HelmetProvider>
+      <Helmet>
+        <title> YUL | {titleName}</title>
+      </Helmet>
+    </HelmetProvider>
+    // react-helmet 오류 이슈로 삭제 후 react-helmet-async를 재설치
+    // 헬맷 부모로 HelmetProvider를 감싸면 끝
   );
 };
--- src/pages/detail/Detail.js
+++ src/pages/detail/Detail.js
@@ -1,21 +1,48 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { movieDetail } from "../../api";
+import styled from "styled-components";
+
+const Wrap = styled.div`
+  padding: 150px 5%;
+`;
+
+const MainImg = styled.div``;
+const TitleName = styled.div``;
+const VoteAverage = styled.div``;
+const Genres = styled.div``;
+const ReleaseData = styled.div``;
+const Runtime = styled.div``;
 
 export const Detail = () => {
   const { id } = useParams();
   // 아이디 값 찾기
+  const [detailData, setDetailData] = useState();
 
   useEffect(() => {
     (async () => {
       try {
         const data = await movieDetail(id);
-        console.log(data);
+        setDetailData(data);
+        // console.log(data);
       } catch (error) {
         console.log("Error :" + error);
       }
     })();
   }, []);
 
-  return <>detail</>;
+  return (
+    <Wrap>
+      <MainImg />
+      <TitleName>{detailData.title}</TitleName>
+      <VoteAverage>{Math.round(detailData.vote_average)}점</VoteAverage>
+      {/* <Genres>
+        {detailData.genres.map((genre) => {
+          // <li>{genre.name}</li>;
+        })}
+      </Genres> */}
+      <ReleaseData>{detailData.release_date}</ReleaseData>
+      <Runtime>{detailData.runtime}분</Runtime>
+    </Wrap>
+  );
 };
